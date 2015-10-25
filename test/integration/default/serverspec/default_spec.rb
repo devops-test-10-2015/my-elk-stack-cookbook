b82e31295821dbe5dd5dc5533a086c2e8dea9015
test user <kevin@kevin-fedora.Home>
2015-10-25 12:31:24 +0000

    About to generate an attribute.

--- test/integration/default/serverspec/default_spec.rb
+++ test/integration/default/serverspec/default_spec.rb
@@ -1,20 +1,23 @@
 require 'spec_helper'
 
-describe 'my-elk-stack-cookbook::default' do
-  # Serverspec examples can be found at
-  # http://serverspec.org/resource_types.html
-  it 'does something' do
-    skip 'Replace this with meaningful tests'
-  end
-end
+#describe 'my-elk-stack-cookbook::default' do
+#  # Serverspec examples can be found at
+#  # http://serverspec.org/resource_types.html
+#  it 'does something' do
+#    skip 'Replace this with meaningful tests'
+#  end
+#end
 
 describe user('root') do
   it { should exist }
 end
 
-describe user('elkuser') do
+app_user_name = "elkuser"
+describe user(app_user_name) do
   it { should exist }
-  it { should have_home_directory '/home/elkuser' }
+  it { should have_home_directory "/home/#{app_user_name}" }
+  it { should have_login_shell '/bin/bash' }
 end
+
 
 
