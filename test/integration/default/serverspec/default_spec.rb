44ebd0e89b9608666d1ca28ad4f91594c2bd879d
test user <kevin@kevin-fedora.Home>
2015-10-25 12:09:56 +0000

    First actual tests are working.

--- test/integration/default/serverspec/default_spec.rb
+++ test/integration/default/serverspec/default_spec.rb
@@ -7,3 +7,14 @@ describe 'my-elk-stack-cookbook::default' do
     skip 'Replace this with meaningful tests'
   end
 end
+
+describe user('root') do
+  it { should exist }
+end
+
+describe user('elkuser') do
+  it { should exist }
+  it { should have_home_directory '/home/elkuser' }
+end
+
+
