9db4ff963d8180da93c92d9536da65f5f15bb69f
test user <kevin@kevin-fedora.Home>
2015-10-25 16:28:34 +0000

    I think that all code is in place but I have not been able to test it as Docker does not play nicely with systemd so I can't get the services to start. Going to try with vagrant.

--- recipes/default.rb
+++ recipes/default.rb
@@ -7,6 +7,8 @@
 package 'java-1.8.0-openjdk-devel'
 package 'curl'
 package 'logrotate'
+package 'systemd'
+#package 'systemd-libs'
 
 user node[:elk][:user] do
   comment 'Running Elastic Search, Logstash and Kibana'
@@ -21,7 +23,11 @@ directory node[:elk][:user] do
   action :create
 end
 
+################################################################################
+# Logstash installation
+
 ls_rpm_temp_loc = '/tmp/logstash.rpm' 
+
 remote_file ls_rpm_temp_loc do
   source "https://download.elastic.co/logstash/logstash/packages/centos/logstash-#{node[:elk][:logstash][:version]}-1.noarch.rpm"
   checksum node[:elk][:logstash][:checksum] 
@@ -35,4 +41,36 @@ rpm_package 'logstash' do
   source ls_rpm_temp_loc
 end
 
+################################################################################
+# Elastic Search installation
+
+es_rpm_temp_loc = '/tmp/elasticsearch.rpm'
+
+remote_file es_rpm_temp_loc do
+  source "https://download.elastic.co/elasticsearch/elasticsearch/elasticsearch-#{node[:elk][:elasticsearch][:version]}.noarch.rpm"
+  checksum node[:elk][:elasticsearch][:checksum] 
+  owner node[:elk][:user] 
+  group node[:elk][:user] 
+  mode '0755'
+  action :create
+end
+
+rpm_package 'elasticsearch' do
+  source es_rpm_temp_loc
+end
+
+service 'elasticsearch'
+# It seems that chef does not yet support systemd
+#service 'elasticsearch' do
+#  supports :status => true, :restart => true, :reload => true
+#  action [ :enable, :start ]
+#end
+
+execute 'enable_elasticsearch' do
+  command 'systemctl enable elasticsearch'
+end
+
+execute 'start_elasticsearch' do
+  command 'systemctl start elasticsearch'
+end
 
